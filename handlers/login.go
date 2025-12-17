131dc6a877963142719dd3fa00eeb5a349382bf4
safeya11 <safiya@authnull.com>
2025-12-17 15:22:53 +0530

    changed amr and acr values

--- handlers/login.go
+++ handlers/login.go
@@ -1619,7 +1619,7 @@ func SignAndPostJWT(w http.ResponseWriter, r *http.Request, username, redirectUR
 			Subject:   sub,
 		},
 		Acr:               "urn:schemas:Microsoft:authenticationmethod:external",
-		Amr:               []string{"otp"}, // Fingerprint, otp or other factor used by your platform
+		Amr:               []string{"mfa"}, // Fingerprint, otp or other factor used by your platform
 		PreferredUsername: username,
 		Nonce:             nonce,
 	}
