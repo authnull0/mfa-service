da804284327207a0bdb53678bc46a39eee26db4d
safeya11 <safiya@authnull.com>
2025-12-17 15:52:02 +0530

    commented amr values

--- handlers/login.go
+++ handlers/login.go
@@ -1619,7 +1619,7 @@ func SignAndPostJWT(w http.ResponseWriter, r *http.Request, username, redirectUR
 			Subject:   sub,
 		},
 		Acr:               "urn:schemas:Microsoft:authenticationmethod:external",
-		Amr:               "mfa", // Fingerprint, otp or other factor used by your platform
+		Amr:               "otp", // Fingerprint, otp or other factor used by your platform
 		PreferredUsername: username,
 		Nonce:             nonce,
 	}
