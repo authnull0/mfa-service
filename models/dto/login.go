76e78e478c244e54a1f9925d64ef1c7e05a11bfd
safeya11 <safiya@authnull.com>
2026-01-09 09:37:19 +0530

    required: nextfactor param

--- models/dto/login.go
+++ models/dto/login.go
@@ -5,7 +5,7 @@ type NormalLoginRequest struct {
 	Password   string `json:"password"`
 	Url        string `json:"url"`
 	RequestID  string `json:"requestId"`
-	NextFactor string `json:"nextFactor"`
+	NextFactor string `json:"nextFactor" binding:"required"`
 }
 
 type NormalLoginResponse struct {
