1158fcc4cb62ed84e415b6ce104b736d5d5f86d1
safeya11 <safiya@authnull.com>
2026-03-05 14:56:58 +0530

    passkey implmenatation fixes

--- config/config.go
+++ config/config.go
@@ -1,16 +1,21 @@
 package config
 
-import "github.com/go-webauthn/webauthn/webauthn"
+import (
+	"log"
+
+	"github.com/go-webauthn/webauthn/webauthn"
+)
 
 func SetupWebAuthn(rpDisplayName, rpID, rpOrigin string) *webauthn.WebAuthn {
 	wconfig := &webauthn.Config{
 		RPDisplayName: rpDisplayName,
 		RPID:          rpID,
-		RPOrigins:     []string{rpOrigin},
+		RPOrigins:     []string{"https://sscdev.authnull.com"},
 	}
 	webAuthn, err := webauthn.New(wconfig)
 	if err != nil {
 		panic("failed to create webauthn instance: " + err.Error())
 	}
+	log.Default().Printf("Successfully setup WebAuthn values : %v", webAuthn)
 	return webAuthn
 }
